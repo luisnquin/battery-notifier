eee966dc284fa95beb2e1978aec4c84401a639f4
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-28 08:00:55 -0500

    feat(cli): added app description

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+/// A customizable battery notifier for Linux kernels focused in BAT0 and BAT1
 pub struct Args {
     #[arg(short, long)]
     /// To simulate battery states (yaml).
