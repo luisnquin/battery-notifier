e17ad22b7547f2dff7b935220cf3b7275e680352
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-28 08:01:20 -0500

    feat(cli): added styles for help command

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,9 @@
-use clap::Parser;
+use anstyle::{AnsiColor, Color, Style};
+use clap::{builder::Styles, Parser};
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+#[command(styles=get_styles())]
 /// A customizable battery notifier for Linux kernels focused in BAT0 and BAT1
 pub struct Args {
     #[arg(short, long)]
@@ -9,4 +12,38 @@ pub struct Args {
     /// The config file path (toml).
     #[arg(short, long)]
     pub config_file: Option<String>,
+}
+
+fn get_styles() -> Styles {
+    Styles::styled()
+        .usage(
+            Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(Color::Ansi(AnsiColor::BrightCyan))),
+        )
+        .header(
+            Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(Color::Ansi(AnsiColor::BrightCyan))),
+        )
+        .literal(Style::new().fg_color(Some(Color::Ansi(AnsiColor::Green))))
+        .invalid(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::Red))),
+        )
+        .error(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::Red))),
+        )
+        .valid(
+            Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(Color::Ansi(AnsiColor::Green))),
+        )
+        .placeholder(Style::new().fg_color(Some(Color::Ansi(AnsiColor::Black))))
 }
