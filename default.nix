9467b1e8e836e990ce1f6827e15a6534c8258e17
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-30 14:09:08 -0500

    nix(package): program is no longer depending on notify-send(libnotify)

--- default.nix
+++ default.nix
@@ -1,38 +1,30 @@
-{pkgs ? import <nixpkgs> {}, ...}: let
-  runtimePackages = with pkgs; [libnotify];
-in
-  pkgs.rustPlatform.buildRustPackage rec {
-    pname = "battery-notifier";
-    version = "unstable";
+{pkgs ? import <nixpkgs> {}, ...}:
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "battery-notifier";
+  version = "unstable";
 
-    src = builtins.path {
-      name = "${pname}-source";
-      path = ./.;
+  src = builtins.path {
+    name = "${pname}-source";
+    path = ./.;
+  };
+
+  nativeBuildInputs = with pkgs; [git cmake makeWrapper];
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+
+    outputHashes = {
+      "soloud-1.0.5" = "sha256-2Cd5aWfntRawxRSdy+4tJJdTkTeii1ilshQadG6Pybw=";
     };
+  };
 
-    nativeBuildInputs = with pkgs; [git cmake makeWrapper];
-    buildInputs = runtimePackages;
+  preConfigure = ''
+    substituteInPlace ./src/notify.rs \
+    --replace './assets/battery-danger.png' '${placeholder "out"}/assets/battery-danger.png'
+  '';
 
-    cargoLock = {
-      lockFile = ./Cargo.lock;
-
-      outputHashes = {
-        "soloud-1.0.5" = "sha256-2Cd5aWfntRawxRSdy+4tJJdTkTeii1ilshQadG6Pybw=";
-      };
-    };
-
-    preConfigure = ''
-      substituteInPlace ./src/notify.rs \
-      --replace './assets/battery-danger.png' '${placeholder "out"}/assets/battery-danger.png'
-    '';
-
-    preInstall = ''
-      mkdir -p $out/assets/
-      cp ./assets/*.png $out/assets/
-    '';
-
-    postInstall = ''
-      wrapProgram ${placeholder "out"}/bin/${pname} \
-        --prefix PATH : ${pkgs.lib.makeBinPath runtimePackages}
-    '';
-  }
+  preInstall = ''
+    mkdir -p $out/assets/
+    cp ./assets/*.png $out/assets/
+  '';
+}
